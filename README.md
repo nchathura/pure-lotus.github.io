e2549f1a0b075d9be960462d385825628a0416a6
Nchathura <52320494+nchathura@users.noreply.github.com>
2019-12-09 02:41:49 +0530

    Update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pure-lotus.github.io
- 
+
+# WHAT PURE LOTUS IS?
+- Specially designed for non-profitable charities
+- You can download, edit, use this template for free
+
+# Contact Us
+- If you have any question drop us an e-mail to contact@1kreation.com
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@ $(function() {
 
 $('.carousel').carousel();
 
-$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
+// $(window).load(function() {
+// 		// Animate loader off screen
+// 		$(".se-pre-con").fadeOut("slow");;
+// 	});
